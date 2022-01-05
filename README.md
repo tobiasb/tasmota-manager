3d5b300d27e38448a97dee5985456dc6faa19c1c
Tobias Boehm <boehm.tobi@gmail.com>
2022-01-04 21:21:00 -0800

    Init

---
+++ README.md
@@ -0,0 +1,51 @@
+# Manage your devices running Tasmota
+
+Allows you to apply a configuration to your Tasmota devices without necessarily knowing their IP addresses. Supports
+
+- Individual devices (if you do know the IP address)
+- Bulk update of devices within the network
+
+## Getting started
+
+```
+$ pipenv install
+$ pipenv run python manage-tasmotas.py --help
+
+Usage: manage-tasmotas.py [OPTIONS]
+
+Options:
+  --ip TEXT      IP address of individual device to update
+  --config TEXT  Path to configuration file  [required]
+  --cidr TEXT    CIDR to scan for Tasmota devices to update
+  --help         Show this message and exit.
+```
+
+The config file supports default values that apply to all devices as well as device specific overrides. It looks like this:
+
+```
+{
+    "default": {
+        "mqtthost": "...",
+        "mqttport": "1883",
+        "mqttuser": "ha",
+        "mqttpassword": "...",
+        "topic": "%06X",
+        "fulltopic": "tasmota/%prefix%/%topic%/",
+        "TelePeriod": "10",
+        "latitude": "48.123",
+        "longitude": "-124.123",
+        "timezone": "99",
+        "TimeDst": "0,0,3,1,2,-420",
+        "TimeStd": "0,0,10,1,3,-480",
+        "ntpserver": "pool.ntp.org",
+        "SetOption65": "1",
+        ...
+    },
+    "<Tasmota friendly-name>": {
+        "PowerOnState": "0",
+        ...
+    }
+}
+```
+
+Use the Tasmota friendly-name as the key when overriding configuration values for a specific device.
---
+++ manage-tasmotas.py
@@ -0,0 +1,46 @@
+import json
+from urllib.parse import quote_plus
+
+import click
+import requests
+import ipaddress
+
+def update_by_ip(ip, configs):
+    try:
+        res = requests.get(url=f'http://{ip}/cm?cmnd=' 'DeviceName' '', timeout=(1, 5))
+        if res.status_code >= 300:
+            print(f'{ip} returned {res.status_code}')
+            return
+        device_name = res.json()["DeviceName"]
+        device_id = device_name.split(' ')[-1]
+
+        config = {**configs['default'], **configs.get(device_id, {})}
+        cmd = quote_plus(f'Backlog {" ".join([ f"{k} {v};" for k,v in config.items() ])}')
+
+        res = requests.post(url=f'http://{ip}/cm?cmnd={cmd}')
+
+        if res.status_code == 200:
+            print(f'{device_name} at {ip} updated!')
+        else:
+            print(f'{device_name} at {ip} responded with {res.status_code}')
+    except Exception:
+        print(f'{ip} unreachable')
+
+
+@click.command()
+@click.option('--ip', default=None, help='IP address of individual device to update')
+@click.option('--config', required=True, help='Path to configuration file')
+@click.option('--cidr', default='192.168.1.0/24', help='CIDR to scan for Tasmota devices to update')
+def update(ip, config, cidr):
+    with open(config, 'r') as f:
+        configs = json.loads(f.read())
+
+    if ip:
+        update_by_ip(ip, configs)
+    else:
+        for ip in ipaddress.IPv4Network(cidr):
+            update_by_ip(str(ip), configs)
+
+
+if __name__ == '__main__':
+    update()
